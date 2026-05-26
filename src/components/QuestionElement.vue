9e65426f330d63fa0dc5e5a360193a70f987b1bc
Mydlitor <jakub.sputo@gmail.com>
2026-05-26 16:27:37 +0200

    feat: change the question text color based on the question answer

--- src/components/QuestionElement.vue
+++ src/components/QuestionElement.vue
@@ -1,25 +1,41 @@
 <template>
     <q-card class="question-element">
-        <p>{{ props.question.text }}</p>
+        <p class="points-text" v-if="isText">{{ props.question.points }}</p>
+        <p class="x-symbol" v-else>X</p>
     </q-card>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-const textColor = ref({
-    color: 'white',
-});
+import { computed } from 'vue';
 
 const props = defineProps({
     question: Object
 });
+
+const isText = computed(() => {
+    return props.question.isAnswered !== false
+})
+
+const textColor = computed(() => {
+    if (props.question.teamAnswered !== null)
+        return { color: props.question.teamAnswered.color }
+    return { color: 'white' }
+})
+
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 p {
     font-weight: 700;
     font-size: xx-large;
+}
+
+.points-text {
     color: v-bind('textColor.color')
+}
+
+.x-symbol {
+    color: $muted;
 }
 
 .question-element {
