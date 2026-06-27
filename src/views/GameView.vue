d4768e337165ac1e76030ef15913ef9669e9fe23
Mydlitor <jakub.sputo@gmail.com>
2026-06-27 14:07:18 +0200

    fix: tweak team changing logic

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -8,79 +8,97 @@
             <p>NO QUESTIONS</p>
         </div>
         <div class="questions-container">
-            <div class="category-column" v-for="category in questions.categories" v-bind:key="category.name">
-                <p class="category-name" style="font-size: large; word-break: break-word; height: 3rem;">
-                    {{ category.name }}</p>
-                <QuestionElement v-for="question in category.questions" v-bind:key="question.number"
-                    :question="question" @click="showQuestionDialog(question)" />
+            <div
+                class="category-column"
+                v-for="category in questions.categories"
+                v-bind:key="category.name"
+            >
+                <p
+                    class="category-name"
+                    style="font-size: large; word-break: break-word; height: 3rem"
+                >
+                    {{ category.name }}
+                </p>
+                <QuestionElement
+                    v-for="question in category.questions"
+                    v-bind:key="question.number"
+                    :question="question"
+                    @click="showQuestionDialog(question)"
+                />
             </div>
         </div>
-        <q-btn v-show="isGameResultEnded" label="SHOW RESULTS" @click="isGameResultDialogShown = true" />
+        <span>{{ currentTeam }}</span>
+        <q-btn
+            v-show="isGameResultEnded"
+            label="SHOW RESULTS"
+            @click="isGameResultDialogShown = true"
+        />
 
-        <QuestionDialog v-model="isQuestionDialogShown" :question="currentQuestion" :teams="teams"
-            :current-team="currentTeam" :answer-time="answerTime" @answered-correctly="onAnsweredCorrectly"
-            @answered-incorrectly="onAnsweredIncorrectly" @hide-dialog="onDialogHide" />
+        <QuestionDialog
+            v-model="isQuestionDialogShown"
+            :question="currentQuestion"
+            :teams="teams"
+            :current-team="currentTeam"
+            :answer-time="answerTime"
+            @answered-correctly="onAnsweredCorrectly"
+            @answered-incorrectly="onAnsweredIncorrectly"
+            @hide-dialog="onDialogHide"
+        />
         <GameResultDialog v-model="isGameResultDialogShown" :teams="teams" />
-
     </q-page>
 </template>
 
 <script setup>
-import { useGameStore } from 'src/stores/gameStore';
-import { computed, onMounted, ref, watch } from 'vue';
-import { useRouter } from 'vue-router';
-import QuestionElement from 'components/QuestionElement.vue';
-import QuestionDialog from 'src/components/QuestionDialog.vue';
-import GameResultDialog from 'src/components/GameResultDialog.vue';
+import { useGameStore } from "src/stores/gameStore";
+import { computed, onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import QuestionElement from "components/QuestionElement.vue";
+import QuestionDialog from "src/components/QuestionDialog.vue";
+import GameResultDialog from "src/components/GameResultDialog.vue";
 
 const gameStore = useGameStore();
 const router = useRouter();
 
-const questions = computed(() => gameStore.getQuestions())
-const teams = computed(() => gameStore.getTeams())
-const answerTime = computed(() => gameStore.getAnswerTime())
-const isQuestionDialogShown = ref(false)
-const isGameResultEnded = computed(() => gameStore.getGameEndedStatus())
+const questions = computed(() => gameStore.getQuestions());
+const teams = computed(() => gameStore.getTeams());
+const answerTime = computed(() => gameStore.getAnswerTime());
+const isQuestionDialogShown = ref(false);
+const isGameResultEnded = computed(() => gameStore.getGameEndedStatus());
 const isGameResultDialogShown = ref(false);
-const currentQuestion = ref(null)
-const currentTeam = ref(null)
+const currentQuestion = ref(null);
+const currentTeam = ref(null);
+const answered = ref(false);
 
 const showQuestionDialog = (question) => {
+    if (currentQuestion.value != null && answered.value) {
+        gameStore.selectNextTeam();
+    }
     currentQuestion.value = question;
-    currentTeam.value = gameStore.getCurrentTeam()
+    currentTeam.value = gameStore.getCurrentTeam();
     isQuestionDialogShown.value = true;
-}
+    answered.value = false;
+};
 
 const onAnsweredCorrectly = (question, team) => {
-    gameStore.setQuestionCorrect(question, team)
-}
+    gameStore.setQuestionCorrect(question, team);
+    answered.value = true;
+};
 
 const onAnsweredIncorrectly = (question) => {
-    gameStore.setQuestionIncorrect(question)
-}
-
-watch((currentQuestion), (oldQuestion, newQuestion) => {
-    if (oldQuestion == null)
-        return;
-    if (oldQuestion != newQuestion)
-        gameStore.selectNextTeam()
-})
-
-// const onDialogHide = () => {
-//     gameStore.selectNextTeam()
-// }
+    gameStore.setQuestionIncorrect(question);
+    answered.value = true;
+};
 
 const onBackButton = () => {
     gameStore.saveGameStatus();
     router.push("/");
-}
+};
 
 onMounted(() => {
     gameStore.setupData();
     currentTeam.value = gameStore.getCurrentTeam();
-    console.log(isGameResultEnded.value)
-})
-
+    console.log(isGameResultEnded.value);
+});
 </script>
 
 <style scoped>
